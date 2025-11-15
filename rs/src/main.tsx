24303b2cbd83b1ddd0dcc5373a49ee72e16ad828
atsoc1993 <atsoc1993@gmail.com>
2025-11-14 20:16:46 -0500

    Creating a load state in main.tsx

--- rs/src/main.tsx
+++ rs/src/main.tsx
@@ -5,15 +5,30 @@ import CraftingPage from './pages/CraftingPage.tsx'
 import BattlePage from './pages/BattlePage.tsx'
 import CharacterPage from './pages/CharacterPage.tsx'
 import ResourcesPage from './pages/ResourcesPage.tsx'
+import { items, type CharacterSet } from './pages/constants.tsx'
 
-createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
+
+function Main() {
+  const character: CharacterSet = {
+    helm: items.helms.dragon_helm,
+    body: items.bodies.bronze_platebody,
+    legs: items.legs.mithril_platelegs,
+    boots: items.boots.rune_boots,
+    sword: items.swords.dragon_sword,
+    shield: items.shields.dragon_shield
+  }
+
+  
+  return (
+    <BrowserRouter>
     <Routes>
-      <Route path='/' element={<CharacterPage />}/>
+      <Route path='/' element={<CharacterPage characterSet={character}/>}/>
       <Route path='/resources' element={<ResourcesPage />}/>
       <Route path='/crafting' element={<CraftingPage />}/>
       <Route path='/battle' element={<BattlePage />}/>
       <Route path='/market' element={<MarketPage />}/>
     </Routes>
   </BrowserRouter>
-)
+  )
+}
+createRoot(document.getElementById('root')!).render(<Main/>)
