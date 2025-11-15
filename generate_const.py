735d9fc5b9544130ffd61cbaa97bfae940739bd9
atsoc1993 <atsoc1993@gmail.com>
2025-11-14 20:15:42 -0500

    File to generate constants more quickly

---
+++ generate_const.py
@@ -0,0 +1,22 @@
+# import os
+
+# files = os.listdir(path='rs/public')
+# print('{')
+
+# all_file_strings = []
+# types = [
+#     'helm',
+#     'platebody',
+#     'platelegs',
+#     'shield',
+#     'sword',
+#     'boots'
+# ]
+# for type in types:
+#     for file in files:
+#         if type in file:
+#             print(f"    {file.split('.')[0]}: {file.split('.')[0].upper()},")
+# print('}')
+
+# # for file in files:
+# #     print(f"const {file.split('.')[0].upper()} = '{file}'")
