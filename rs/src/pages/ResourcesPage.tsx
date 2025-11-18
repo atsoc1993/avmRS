9fedd101e72a43b5366ddbc341b03f6fad94f274
atsoc1993 <atsoc1993@gmail.com>
2025-11-18 15:28:58 -0500

    Added resources for wood and ore to resources page

--- rs/src/pages/ResourcesPage.tsx
+++ rs/src/pages/ResourcesPage.tsx
@@ -1,14 +1,35 @@
 import NavBar from "../components/NavBar";
 import NavBarSpacer from "../components/NavBarSpacer";
+import { resources } from "../constants";
 
 export default function ResourcesPage() {
+
     return (
         <>
-            <NavBar selected="resources"/>
+            <NavBar selected="resources" />
             <NavBarSpacer />
-            <h1>
-                Resources
-            </h1>
+
+            <h1 className="text-4xl text-center m-10">Mining</h1>
+            <div className="flex flex-row justify-evenly">
+
+                {Object.values(resources.ore).map((ore) => (
+                    <div className="bg-amber-300 flex flex-col p-6 rounded-2xl shadow-2xl shadow-black border-2 w-50">
+                        <h1 className="text-center">{ore.name}</h1>
+                        <img className='w-20 h-20 place-self-center' src={ore.image_url} />
+                    </div>
+                ))}
+            </div>
+            <h1 className="text-4xl text-center m-10">Woodcutting</h1>
+            <div className="flex flex-row justify-evenly">
+
+                {Object.values(resources.wood).map((wood) => (
+                    <div className="bg-amber-300 flex flex-col p-6 rounded-2xl shadow-2xl shadow-black border-2 w-50">
+                        <h1 className="text-center">{wood.name}</h1>
+                        <img className='w-20 h-20 place-self-center' src={wood.image_url} />
+                    </div>
+                ))}
+            </div>
+
         </>
     )
 }
